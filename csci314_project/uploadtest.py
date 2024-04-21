266571416d4b8ae74fa90e5722a25ac1364b6322
daffybum <160607897+daffybum@users.noreply.github.com>
2024-04-21 15:12:46 +0800

    Update uploadtest.py

--- csci314_project/uploadtest.py
+++ csci314_project/uploadtest.py
@@ -8,14 +8,6 @@ conn = mysql.connector.connect(
 )
 cursor = conn.cursor()
 
-# Read image files and insert image data into the database
-image_paths = ['visualex/static/images/cat_or_dog_1.jpg', 'visualex/static/images/cat_or_dog_2.jpg']
-for image_path in image_paths:
-    with open(image_path, 'rb') as file:
-        image_data = file.read()
-        # Insert image data into image_metadata table
-        cursor.execute("INSERT INTO image_metadata (image_data) VALUES (%s)", (image_data,))
 
-# Commit changes and close connection
 conn.commit()
 conn.close()

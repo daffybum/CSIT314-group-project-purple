f969ee55b41720f8a89c5df070b1d8c9cfa354c7
daffybum <160607897+daffybum@users.noreply.github.com>
2024-05-03 16:32:00 +0800

    Update boundary.py

--- csci314_project/boundary.py
+++ csci314_project/boundary.py
@@ -20,7 +20,7 @@ def login():
         else:
             flash('Wrong password or username', category='error')
 
-    return render_template("login.html", boolean=True) 
+    return render_template("login.html", boolean=True)
 
 @boundary.route('/logout')
 def logout():
@@ -30,12 +30,12 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         role = request.form.get('role')
-        email = request.form.get('email')
         username = request.form.get('username')
         name = request.form.get('name')
         surname = request.form.get('surname')
         contact = request.form.get('contact')
         date_of_birth = request.form.get('date_of_birth')
+        email = request.form.get('email')
         address = request.form.get('address')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
@@ -51,7 +51,7 @@ def sign_up():
         else:
             createAccountController = controller.CreateUserAccController()
             password1 = generate_password_hash(password1, method='pbkdf2')
-            userAcc = entity.UserAccount(role,username, password1, name, surname, contact, email, date_of_birth, address)
+            userAcc = entity.UserAccount(role,username, password1, name, surname, contact, date_of_birth, email, address)
             result = createAccountController.createUserAccount(userAcc)
             if (result):
                 flash('Account created!', category='success')
@@ -72,14 +72,97 @@ def display_profile():
         display= controller.DisplayController()
         user = display.get_user_info(username)
         if user:
-            role,username,name, surname, contact, date_of_birth, email, address = user
-            return render_template("accountdetail.html", role=role,username=username,name=name, surname=surname,contact=contact,date_of_birth=date_of_birth, email=email, address=address,user_name = username)
+            role, username, name, surname, contact, date_of_birth, email, address = user
+            return render_template("accountdetail.html", role = role,username=username, name=name, surname=surname,contact=contact,date_of_birth=date_of_birth, email=email, address=address,user_name = username)
         else:
             flash('User not found', category='error')
             return redirect(url_for('boundary.login'))
     else:
         flash('User not logged in', category='error')
         return redirect(url_for('boundary.login'))
+
+
+@boundary.route('/editprofile', methods=['GET', 'POST'])
+def editProfile():
+    username = session.get('username')
+    role = session.get('role')
+    selected_user = session.get('selected_user')
+    display= controller.DisplayController()
+
+    
+    if role == 'admin':
+        user = display.get_user_info(selected_user)
+        role,selected_user,name, surname,contact, email, date_of_birth, address = user
+        if request.method == 'POST':
+            role1 = request.form.get('role')
+            username1 = request.form.get('username')
+            name1 = request.form.get('name')
+            surname1 = request.form.get('surname')
+            contact1 = request.form.get('contact')
+            date_of_birth1 = request.form.get('date_of_birth')
+            email1 = request.form.get('email')
+            address1 = request.form.get('address')
+            editProfileController = controller.EditProfileController()
+            editProfile = editProfileController.edit_profile(selected_user, role1,username1, name1, surname1, contact1,date_of_birth1,email1, address1)
+            if editProfile:
+                flash('Profile updated successfully')
+            else:
+                flash('Cannot update profile')
+        return render_template("editprofile.html", role = role,username=selected_user,name=name, surname=surname,contact=contact, date_of_birth=date_of_birth,email=email,address=address, user_name = username)
+    else:
+        user = display.get_user_info(username)
+        role,username,name, surname, contact, date_of_birth,email, address= user
+        if request.method == 'POST':
+            name1 = request.form.get('name')
+            surname1 = request.form.get('surname')
+            contact1 = request.form.get('contact')
+            date_of_birth1 = request.form.get('date_of_birth')
+            email1 = request.form.get('email')
+            address1 = request.form.get('address')
+            editProfileController = controller.EditProfileController()
+            editProfile = editProfileController.edit_profile1( name1, surname1,contact1, date_of_birth1,email1, address1,username)
+            if editProfile:
+                flash('Profile updated successfully')
+            else:
+                flash('Cannot update profile')
+        return render_template("editprofile.html", role=role,username=selected_user,name=name, surname=surname, contact=contact,date_of_birth= date_of_birth, email=email, address=address, user_name = username)
+
+    
+
+
+#ADMIN
+@boundary.route('/viewAllUsers')
+def viewAllUsers():
+    username = session.get('username')
+    getAllUserController = controller.GetAllUsersController()
+    users_list = getAllUserController.get_all_users()
+    return render_template('viewAllUsersAccount.html', user_name = username, users_list = users_list, search_exist = False)
+
+# If admin clicks view button
+@boundary.route('/viewUserDetails', methods=['POST'])
+def viewUserDetails():
+    username = session.get('username')
+    selected_user = request.form['selectedUsername']
+    display= controller.DisplayController()
+    user = display.get_user_info(selected_user)
+    if user:
+        role,selected_user,name, surname,contact, email, address = user
+        return render_template("accountdetail.html", role=role,username=selected_user,name=name, surname=surname,contact=contact, email=email, address=address, user_name = username)
+    
+# If admin clicks search
+@boundary.route('/viewSearchedUserDetails', methods=['POST'])
+def viewSearchedUserDetails():
+    username = session.get('username')
+    inputUsername = request.form.get('inputUsername')
+    searchUserController = controller.SearchUserController()
+    searchedUsername = searchUserController.search_user(inputUsername)
+    if searchedUsername is None:
+        flash('Username does not exist!', category='error')
+        getAllUserController = controller.GetAllUsersController()
+        users_list = getAllUserController.get_all_users()
+        return render_template('viewAllUsersAccount.html', user_name = username, users_list = users_list, search_exist = False)
+    else:
+        return render_template('viewAllUsersAccount.html', user_name = username, users = searchedUsername ,search_exist = True)
     
 @boundary.route('/about-us')
 def aboutus():
